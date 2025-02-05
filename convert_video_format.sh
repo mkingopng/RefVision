3f671af0990189930f9d0744adf312b7a75fe8eb
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2025-02-06 01:59:13 +1000

    its working. got rid of obsolete shell scripts. tried to clean up the project because its messy but make problems for myself so I've rolled it back

--- convert_video_format.sh
+++
@@ -1,9 +0,0 @@
-# convert_video_format.sh
-poetry run python src/main.py --video "$VIDEO_FILE" --model_path "$MODEL_PATH"
-AVI_FILE="runs/pose/track2/chris_kennedy_squat.avi"
-MP4_FILE="runs/pose/track2/chris_kennedy_squat.mp4"
-
-if [ -f "$AVI_FILE" ]; then
-    ffmpeg -y -i "$AVI_FILE" -c:v libx264 -pix_fmt yuv420p "$MP4_FILE"
-    rm "$AVI_FILE"
-fi
